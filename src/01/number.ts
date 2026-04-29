5efcb5af41af68a382b148475dc4708b92d0a82a
13inuo0 <13inu.o0@gmail.com>
2026-04-29 22:39:05 +0900

    타입 종류

---
+++ src/01/number.ts
@@ -0,0 +1,18 @@
+//숫자
+let num: number = 10;
+// num = "Hello"; //숫자 타입에 텍스트 입력 = 에러 발생
+
+// 문자
+let str: string = "Hello";
+
+// 논리
+let bool: boolean = true;
+
+// Undefined
+let und: undefined = undefined;
+
+// Null
+let nul: null = null;
+
+// symbol
+let sym: symbol = Symbol();
